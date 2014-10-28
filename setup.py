0fb61797cbcf4e1bd81d523a0ef1fa94324bcbd1
Maarten Versteegh <maartenversteegh@gmail.com>
2014-10-28 21:09:36 +0100

    Add ccss.pyx and fix setup.py
    Turns out mixing setuptools and distutils isn't such a great idea.

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+
 import os
 import numpy
-import sys
-from distutils.extension import Extension
-from distutils.sysconfig import *
-from distutils.util import *
+
 from Cython.Distutils import build_ext
 
 try:
-    from setuptools import setup
+    from setuptools import setup, Extension
 except ImportError:
     from distutils.core import setup
+    from distutils.extension import Extension
 
 
 readme = open('README.rst').read()
@@ -26,7 +26,6 @@ test_requirements = [
     # TODO: put package test requirements here
 ]
 
-py_inc = [get_python_inc()]
 np_lib = os.path.dirname(numpy.__file__)
 np_inc = [os.path.join(np_lib, 'core/include')]
 
@@ -57,11 +56,11 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
     ],
-    # cmdclass={'build_ext': build_ext},
-    # ext_modules=[Extension('ccss', ['tde/ccss.pyx'],
-    #                        include_dirs=py_inc + np_inc)],
-    # include_dirs=[numpy.get_include(),
-    #               os.path.join(numpy.get_include(), 'numpy')],
+    cmdclass={'build_ext': build_ext},
+    ext_modules=[Extension('ccss', ['tde/ccss.pyx'],
+                           include_dirs=np_inc)],
+    include_dirs=[numpy.get_include(),
+                  os.path.join(numpy.get_include(), 'numpy')],
     test_suite='tests',
     tests_require=test_requirements
 )
---
+++ tde/ccss.pyx
@@ -0,0 +1,78 @@
+## cython ccss.pyx
+## gcc -shared -pthread -fPIC -fwrapv -O3 -Wall -fno-strict-aliasing -I/usr/include/python2.7 -I/usr/lib/python2.7/site-packages/numpy/core/include -o ccss.so ccss.c
+
+
+import numpy as np
+cimport numpy as np
+cimport cython
+ITYPE = np.long
+ctypedef np.long_t ITYPE_t
+
+
+cdef int triangle(int n):
+    cdef long  r = 0
+    cdef int i
+    for i in xrange(1, n):
+        r += i
+    return r
+
+
+@cython.boundscheck(False)
+def allcommonsubstrings(np.ndarray[ITYPE_t, ndim=1] XA,
+                        np.ndarray[ITYPE_t, ndim=1] XB,
+                        int minlength, int same=0):
+    cdef int mA, mB, i, j, mT, sA, sB, b, score, start, end, max
+    mA = XA.shape[0]
+    mB = XB.shape[0]
+    cdef np.ndarray[ITYPE_t, ndim=2] r
+    cdef ITYPE_t[:] starters
+    cdef np.ndarray[ITYPE_t, ndim=1] tmp
+    cdef ITYPE_t[:, :] diag
+
+    cdef np.ndarray[ITYPE_t, ndim=2] m = np.zeros((mA+1, mB+1), dtype=ITYPE)
+
+    for i in xrange(1, mA+1):
+        for j in xrange(1, mB+1):
+            if same and i == j:
+                continue
+            if XA[i-1] == XB[j-1]:
+                m[i, j] = m[i-1, j-1] + 1
+
+    tmp = np.argsort(m, axis=None)
+    tmp = tmp[np.where(np.ravel(m)[tmp] >= minlength)]
+
+    mT = tmp.shape[0]
+    starters = np.empty((mT,), dtype=ITYPE)
+    for i in xrange(mT):
+        starters[i] = tmp[mT-i-1]
+
+    if starters.shape[0] == 0:
+        return None
+
+    max = m[np.unravel_index(starters[0], dims=(mA+1, mB+1))]
+    r = np.empty((mT*triangle(max), 3), dtype=ITYPE)
+    i = 0
+    while True:
+        sA, sB = np.unravel_index(starters[0], dims=(mA+1, mB+1))
+        score = m[sA, sB]
+
+        # diagonal over m leading up to starter
+        diag = np.empty((score+1, 2), dtype=ITYPE)
+        for j in xrange(score+1):
+            diag[j, 0] = sA - score + j
+            diag[j, 1] = sB - score + j
+
+        for start in xrange(score-minlength+1):
+            for end in xrange(start + minlength, score+1):
+                r[i, 0] = diag[start, 0]
+                r[i, 1] = diag[start, 1]
+                r[i, 2] = end - start
+                i += 1
+        starters = np.fromiter((x for x in starters
+                                if not x in np.ravel_multi_index(diag.T,
+                                                                 dims=(mA+1, mB+1))),
+                               dtype=ITYPE)
+        if starters.shape[0] == 0:
+            break
+
+    return r[:i, :]
