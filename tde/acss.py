3643a36863d4b463fd6d225a93bfebef8496fc77
Maarten Versteegh <maartenversteegh@gmail.com>
2014-12-15 13:21:55 +0100

    added pairwise_substring_completion

--- tde/acss.py
+++ tde/acss.py
@@ -3,9 +3,79 @@
 """
 
 import numpy as np
-import ccss
+from itertools import product, combinations
+from .ccss import allcommonsubstrings as _acss
+from .corpus import Interval, FragmentToken
 
-def allcommonsubstrings(s1, s2=None, minlength=3, same=False):
+def pairwise_substring_completion(fragment1, fragment2, corpus,
+                                  minlength, maxlength):
+    name1, name2 = fragment1.name, fragment2.name
+    tokenseq1 = [(f.mark, f.interval)
+                 for f in corpus.tokens(name1, fragment1.interval)]
+    tokenseq2 = [(f.mark, f.interval)
+                 for f in corpus.tokens(name2, fragment2.interval)]
+    for seq1, seq2 in product(substrings(tokenseq1, minlength, maxlength),
+                              substrings(tokenseq2, minlength, maxlength)):
+        submark1, intervalseq1 = zip(*seq1)
+        submark2, intervalseq2 = zip(*seq2)
+        interval1 = Interval(intervalseq1[0].start, intervalseq1[-1].end)
+        interval2 = Interval(intervalseq2[0].start, intervalseq2[-1].end)
+        yield (FragmentToken(name1, interval1, submark1),
+               FragmentToken(name2, interval2, submark2))
+
+# def pairwise_substring_completion(pair, corpus, minlength, maxlength):
+#     """Generate the substring completions of a pair.
+
+#     Parameters
+#     ----------
+#     pair : ((ClassID, FragmentToken), (ClassID, FragmentToken)) tuple
+#     corpus : Corpus
+
+#     Returns
+#     -------
+#     i : iterator over pairs of ((ClassID, FragmentToken), (ClassID, FragmentToken))
+
+#     """
+#     (classID1, token1), (classID2, token2) = pair
+#     name1 = token1.name
+#     name2 = token2.name
+
+#     tokenseq1 = [(x.mark, x.interval)
+#                  for x in corpus.tokens(name1, token1.interval)]
+#     tokenseq2 = [(x.mark, x.interval)
+#                  for x in corpus.tokens(name2, token2.interval)]
+
+#     for seq1, seq2 in product(substrings(tokenseq1, minlength, maxlength),
+#                               substrings(tokenseq2, minlength, maxlength)):
+#         submark1, intervalseq1 = zip(*seq1)
+#         submark2, intervalseq2 = zip(*seq2)
+#         interval1 = Interval(intervalseq1[0].start, intervalseq1[-1].end)
+#         interval2 = Interval(intervalseq2[0].start, intervalseq2[-1].end)
+
+#         yield ((classID1, FragmentToken(name1, interval1, submark1)),
+#                (classID2, FragmentToken(name2, interval2, submark2)))
+
+
+def substrings(s, minlength, maxlength):
+    """Generate all substrings of s.
+
+    Parameters
+    ----------
+    s : iterable
+    minlength : minimum length of substrings
+
+    Returns
+    -------
+    i : iterator over substrings of s
+
+    """
+    for start, end in combinations(xrange(len(s)+1), 2):
+        if minlength <= end - start <= maxlength:
+            yield s[start: end]
+
+
+
+def allcommonsubstrings(s1, s2=None, minlength=3, maxlength=20, same=False):
     """Find all common substrings.
 
     The algorithm used is a simple dp. This could be sped up by using
@@ -43,7 +113,7 @@ def allcommonsubstrings(s1, s2=None, minlength=3, same=False):
     s1_arr = np.fromiter((sym2idx[s] for s in s1), dtype=np.long)
     s2_arr = np.fromiter((sym2idx[s] for s in s2), dtype=np.long)
 
-    css = ccss.allcommonsubstrings(s1_arr, s2_arr, minlength, same)
+    css = _acss(s1_arr, s2_arr, minlength, maxlength, same)
     if css is None:
         return []
 
