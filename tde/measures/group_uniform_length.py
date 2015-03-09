8f1b14e3a5923031baf51b71959ce321f38ba454
Maarten Versteegh <maartenversteegh@gmail.com>
2015-03-09 23:08:03 +0100

    add weighting schemes
    this commit adds 3 weighting schemes for the matching and grouping
    measures. the weighting schemes are propagated to the english_eval2.py
    script for testing

---
+++ tde/measures/group_uniform_length.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# ------------------------------------
+# file: group.py
+# author:
+# Maarten Versteegh
+# github.com/mwv
+# maartenversteegh AT gmail DOT com
+#
+# Licensed under GPLv3
+# ------------------------------------
+"""group: evaluate grouping score
+
+"""
+
+from __future__ import division
+from pprint import pformat
+
+import numpy as np
+
+from tde.data.sets import Pclus_single, Pgoldclus, typeset, weights, nmatch
+from tde.util.printing import verb_print, banner, pretty_pairs
+from tde.util.functions import intersection
+
+def make_pclus(disc_clsdict, verbose, debug):
+    with verb_print('constructing pclus', verbose, True, True):
+        pclus = list(tuple(sorted((f1, f2),
+                             key=lambda f: (f.name, f.interval.start)))
+                     for f1, f2 in Pclus_single(disc_clsdict))
+    if debug:
+        print banner('PCLUS ({0})'.format(len(pclus)))
+        print pretty_pairs(pclus)
+        print
+    return pclus
+
+def make_pgoldclus(disc_clsdict, verbose, debug):
+    with verb_print('constructing pgoldclus', verbose, True, True):
+        pgoldclus = list(tuple(sorted((f1, f2),
+                                      key=lambda f: (f.name, f.interval.start)))
+                         for f1, f2 in Pgoldclus(disc_clsdict))
+    if debug:
+        pgoldclus = list(pgoldclus)
+        print banner('PGOLDCLUS ({0})'.format(len(pgoldclus)))
+        print pretty_pairs(pgoldclus)
+        print
+    return pgoldclus
+
+def make_typeset(pclus, verbose, debug):
+    with verb_print('constructing typeset', verbose, True, True):
+        ts = list(typeset(pclus))
+    if debug:
+        print banner('TYPESET ({0})'.format(len(ts)))
+        print pformat(ts)
+        print
+    return ts
+
+def make_weights(pclus, verbose, debug):
+    with verb_print('constructing weights', verbose, True, True):
+        ws = weights(pclus)
+    if debug:
+        print banner('WEIGHTS')
+        print pformat(ws)
+        print
+    return ws
+
+def make_pclus_pgoldclus_nmatch(pclus, pgoldclus, verbose, debug):
+    with verb_print('making pclus/pgoldclus nmatch', verbose, True, True):
+        pclus_pgoldclus_intersect = list(intersection(pclus, pgoldclus))
+        pclus_pgoldclus_nmatch = nmatch(pclus_pgoldclus_intersect)
+    if debug:
+        print banner('NMATCH(PCLUS/PGOLDCLUS)')
+        print pformat(pclus_pgoldclus_nmatch)
+        print
+    return pclus_pgoldclus_nmatch
+
+def make_pclus_nmatch(pclus, verbose, debug):
+    with verb_print('constructing pclus nmatch', verbose, True, True):
+        pclus_nmatch = nmatch(pclus)
+    if debug:
+        print banner('NMATCH(PCLUS)')
+        print pformat(pclus_nmatch)
+        print
+    return pclus_nmatch
+
+def make_pgoldclus_nmatch(pgoldclus, verbose, debug):
+    with verb_print('constructing pgoldclus_nmatch', verbose, True, True):
+        pgoldclus_nmatch = nmatch(pgoldclus)
+    if debug:
+        print banner('NMATCH(PGOLDCLUS)')
+        print pformat(pgoldclus_nmatch)
+        print
+    return pgoldclus_nmatch
+
+def evaluate_group(disc_clsdict, verbose=False, debug=False):
+    pclus = make_pclus(disc_clsdict, verbose, debug)
+    pgoldclus = make_pgoldclus(disc_clsdict, verbose, debug)
+
+    ts_disc = make_typeset(pclus, verbose, debug)
+    ts_gold = make_typeset(pgoldclus, verbose, debug)
+
+    pclus_pgoldclus_nmatch = make_pclus_pgoldclus_nmatch(pclus,
+                                                         pgoldclus,
+                                                         verbose, debug)
+    pclus_nmatch = make_pclus_nmatch(pclus, verbose, debug)
+    pgoldclus_nmatch = make_pgoldclus_nmatch(pgoldclus,
+                                             verbose, debug)
+
+    len_disc = sum(len(t) for t in ts_disc)
+    len_gold = sum(len(t) for t in ts_gold)
+
+    if len(pclus_nmatch) == 0:
+        prec = 0.
+    else:
+        prec = sum(len(t) * pclus_pgoldclus_nmatch[t] / pclus_nmatch[t]
+                   for t in ts_disc if pclus_nmatch[t] > 0) / \
+                       (len_disc * len(ts_disc))
+        if not np.isfinite(prec):
+            prec = 0.
+
+    if len(pgoldclus_nmatch) == 0:
+        rec = 0.
+    else:
+        rec = sum(len(t) * pclus_pgoldclus_nmatch[t] / pgoldclus_nmatch[t]
+                  for t in ts_gold if pgoldclus_nmatch[t] > 0) / \
+                      (len_gold * len(ts_gold))
+        if not np.isfinite(rec):
+            rec = 0.
+    return prec, rec
---
+++ tde/measures/match_uniform_length.py
@@ -0,0 +1,137 @@
+"""Evaluate matching score
+
+"""
+
+from __future__ import division
+
+from pprint import pformat
+
+import numpy as np
+
+from tde.data.sets import Pclus, Psubs, nmatch, typeset, weights
+from tde.util.printing import banner, verb_print, pretty_pairs
+from tde.util.functions import intersection
+
+
+def make_pgold(gold_clsdict, verbose, debug):
+    with verb_print('constructing pgold set', verbose, True, True):
+        pgold = list(Pclus(gold_clsdict))
+    if debug:
+        print banner('PGOLD ({0})'.format(len(pgold)))
+        print pretty_pairs(pgold)
+        print
+    return pgold
+
+
+def make_pdisc(disc_clsdict, verbose, debug):
+    with verb_print('constructing pdisc set', verbose, True, True):
+        pdisc = list(Pclus(disc_clsdict))
+    if debug:
+        print banner('PDISC ({0})'.format(len(pdisc)))
+        print pretty_pairs(pdisc)
+        print
+    return pdisc
+
+
+def make_psubs(disc_clsdict, corpus, minlength, maxlength,
+               verbose, debug):
+    with verb_print('constructing psubs set', verbose, True, True):
+        psubs = list(Psubs(disc_clsdict, corpus, minlength=minlength,
+                           maxlength=maxlength))
+    if debug:
+        print banner('PSUBS ({0})'.format(len(psubs)))
+        print pretty_pairs(psubs)
+        print
+    return psubs
+
+
+def make_typeset(psubs, verbose, debug):
+    with verb_print('making typeset', verbose, True, True):
+        ts = list(typeset(psubs))
+    if debug:
+        print banner('TYPES(PSUBS) ({0})'.format(len(ts)))
+        print pformat(ts)
+        print
+    return ts
+
+
+def make_weights(psubs, verbose, debug):
+    with verb_print('making weights', verbose, True, True):
+        ws = weights(psubs)
+    if debug:
+        print banner('WEIGHTS(PSUBS) ({0})'.format(len(ws)))
+        print pformat(ws)
+        print
+    return ws
+
+
+def make_psubs_pgold_nmatch(pgold, psubs, verbose, debug):
+    with verb_print('making psubs/pgold nmatch', verbose, True, True):
+        psubs_pgold_intersect = intersection(pgold, psubs)
+        psubs_pgold_nmatch = nmatch(psubs_pgold_intersect)
+    if debug:
+        print banner('NMATCH(PSUBS/PGOLD)')
+        print pformat(psubs_pgold_nmatch)
+        print
+    return psubs_pgold_nmatch
+
+
+def make_psubs_nmatch(psubs, verbose, debug):
+    with verb_print('making psubs nmatch', verbose, True, True):
+        psubs_nmatch = nmatch(psubs)
+    if debug:
+        print banner('NMATCH(PSUBS)')
+        print pformat(psubs_nmatch)
+        print
+    return psubs_nmatch
+
+
+def make_pgold_nmatch(pgold, verbose, debug):
+    with verb_print('constructing nmatch_gold', verbose, True, True):
+        nmatch_gold = nmatch(pgold)
+
+    if debug:
+        print banner('nmatch_gold')
+        for k, v in nmatch_gold.iteritems():
+            print k, v
+    return nmatch_gold
+
+def eval_from_psets(pdisc, pgold, psubs, verbose=False, debug=False):
+    ts_disc = make_typeset(psubs, verbose, debug)
+    ts_gold = make_typeset(pgold, verbose, debug)
+
+    psubs_pgold_nmatch = make_psubs_pgold_nmatch(pgold, psubs,
+                                                 verbose, debug)
+
+    psubs_nmatch = make_psubs_nmatch(psubs, verbose, debug)
+    pgold_nmatch = make_pgold_nmatch(pgold, verbose, debug)
+
+    len_disc = sum(len(t) for t in ts_disc)
+    len_gold = sum(len(t) for t in ts_gold)
+
+    if len(psubs_nmatch) == 0:
+        prec = 0.
+    else:
+        prec = sum(len(t) * psubs_pgold_nmatch[t] / psubs_nmatch[t]
+                   for t in ts_disc if psubs_nmatch[t] > 0) / \
+                       (len_disc * len(ts_disc))
+        if not np.isfinite(prec):
+            prec = 0.
+
+    if len(pgold_nmatch) == 0:
+        rec = 0.
+    else:
+        rec = sum(len(t) * psubs_pgold_nmatch[t] / pgold_nmatch[t]
+                  for t in ts_gold if pgold_nmatch[t] > 0) / \
+                      (len_gold * len(ts_gold))
+        if not np.isfinite(rec):
+            rec = 0.
+    return prec, rec
+
+def evaluate_matching(disc_clsdict, gold_clsdict, corpus, minlength=3,
+                      maxlength=20, verbose=False, debug=False):
+    pgold = make_pgold(gold_clsdict, verbose, debug)
+    pdisc = make_pdisc(disc_clsdict, verbose, debug)
+    psubs = make_psubs(disc_clsdict, corpus, minlength, maxlength,
+                       verbose, debug)
+    return eval_from_psets(pdisc, pgold, psubs, verbose, debug)
